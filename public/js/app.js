4ef172bed4f82104562bd3ae9d0a830c1cd1117a
Farooqui Rizwan Ahmed <rizwanfarooqui07@gmail.com>
2021-05-08 01:11:54 +0530

    Final commit

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener(`submit`, (e) => {
     messageOne.textContent = `Loading...`
     messsageTwo.textContent = ``
 
-    fetch(`http://localhost:3000/weather?address=`+ location ).then((res) => {
+    fetch(`/weather?address=`+ location ).then((res) => {
     res.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error  
