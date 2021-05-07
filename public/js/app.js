39121010d2dceadaba30e64c6d9f880d02dc626e
Farooqui Rizwan Ahmed <rizwanfarooqui07@gmail.com>
2021-05-07 23:41:58 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log(`Client side javascript file`);
-
-// fetch(`http://localhost:3000/weather?address=lucknow`).then((res) => {
-//     res.json().then((data) => {
-//         if (data.err) {
-//             console.log(data.err);  
-//         } else {
-//             console.log(data.forecast);
-//             console.log(data.location);
-//         }
-//         console.log(data);
-//     })
-// })
 
 const weatherForm = document.querySelector(`form`);
 const search = document.querySelector(`input`);
